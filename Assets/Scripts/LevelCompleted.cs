c5b0235d89c3e277c8deea09ebfbc44e2038fae5
Wojciech Kubiak <282127@uwr.edu.pl>
2018-08-22 06:46:02 +0200

    First map completion

--- Assets/Scripts/LevelCompleted.cs
+++ Assets/Scripts/LevelCompleted.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelCompleted : MonoBehaviour {
-
-	void OnTriggerEnter2D(Collider2D other)
+    public GameObject player;
+    
+    void OnTriggerEnter2D(Collider2D other)
 	{
 		if(other.gameObject.CompareTag("Player"))
 		{
 			GameCtrl.instance.Won ();
 			GameCtrl.instance.timerOn = false;
+            player.SetActive(false);
 		}
 	}
 }
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    Transform trans; //giving Transform a reference
+
+    void Awake() //When game starts...
+    {
+        trans = GetComponent<Transform>(); //return a component to our "trans:
+    }
+
+    void Update()
+    {
+        trans.Rotate(0, 0, -2); //if he is, then rotate clockwise in the x axis
+    }
+}
